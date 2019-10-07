55c7c79837a0102a89189824db825c0c2b91e255
FIAUX Mounir-Yann <Mounir-Yann.FIAUX@cpnv.ch>
2019-10-07 15:59:33 +0200

    07.10.19

--- Ex_5_Dom/Java_Script/code.js
+++ Ex_5_Dom/Java_Script/code.js
@@ -16,15 +16,29 @@ function div3SurvolRien() {
     Arial.classList.remove("Arial")
 }
 
+function div4YES() {
+    if ((BTN_1.checked == true) && (BTN_2.checked == true)){
+        console.log("YES")
+        YES.classList.add("YES")
+        YES.innerHTML = "YES"
+    }
+}
+
+function div5delete() {
+        document.getElementById("sup").remove();
+}
+
 function evenement() {
     L1.addEventListener("click", div1Click);
     L2.addEventListener("click", div2Click);
     Arial.addEventListener("mouseover", div3Survol);
     Arial.addEventListener("mouseleave", div3SurvolRien);
+    BTN_1.addEventListener("click", div4YES);
+    BTN_2.addEventListener("click", div4YES);
+    texte.addEventListener("click", div5delete);
 }
 
-
-
+T UNE BITE MEC !!!!!!!!!!!!!!!!!!
 
 
 
