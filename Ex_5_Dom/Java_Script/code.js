9a7d9883bf788e48c8c4695451d97c22ffdb7f81
FIAUX Mounir-Yann <Mounir-Yann.FIAUX@cpnv.ch>
2019-10-28 15:57:42 +0100

    exercice 5 DOM fini

--- Ex_5_Dom/Java_Script/code.js
+++ Ex_5_Dom/Java_Script/code.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", evenement);
 
 function div1Click() {
-  L1.classList.add("L1")
+    L1.classList.add("L1")
 }
 
 function div2Click() {
@@ -17,7 +17,7 @@ function div3SurvolRien() {
 }
 
 function div4YES() {
-    if ((BTN_1.checked == true) && (BTN_2.checked == true)){
+    if ((BTN_1.checked == true) && (BTN_2.checked == true)) {
         console.log("YES")
         YES.classList.add("YES")
         YES.innerHTML = "YES"
@@ -25,7 +25,56 @@ function div4YES() {
 }
 
 function div5delete() {
-        document.getElementById("sup").remove();
+    document.getElementById("sup").remove();
+}
+
+function divEx6() {
+    var ini = Txt_Name.value.substr(0, 1);
+    var pos = Txt_Name.value.indexOf(" ");
+    if (pos != -1) {
+        ini += Txt_Name.value.substr(pos + 1, 1);
+    }
+    var longueur = Txt_Name.value.length;
+    if (Txt_Name.value.lastIndexOf(" ") != +1 && Txt_Name.value.substr(longueur - 1, 1) != Txt_Name.value.substr(pos + 1, 1)) {
+        ini += Txt_Name.value.substr(longueur - 1, 1);
+    }
+
+    Txt_Init.value = ini.toUpperCase();
+}
+
+function div7Marks() {
+    cmd_Marks.disabled = true
+    cmd_Marks.hidden = false
+    cmd_Go.disabled = true
+    cmd_Go.hidden = false
+    cmd_Ready.disabled = false
+    cmd_Ready.hidden = false
+    Img_start.src = "Image/marks.png"
+}
+
+function div7Ready() {
+    cmd_Marks.hidden = true
+    cmd_Ready.disabled = true
+    cmd_Go.disabled = false
+    Img_start.src = "Image/set.png"
+}
+
+function div7Go() {
+    cmd_Marks.hidden = true
+    cmd_Ready.hidden = true
+    cmd_Go.disabled = true
+    Img_start.src = "Image/go.png"
+    setTimeout(totaux, 3000)
+
+
+}
+
+function totaux() {
+    Img_start.src = "Image/empty.png"
+    cmd_Marks.disabled = false
+    cmd_Marks.hidden = false
+    cmd_Ready.disabled = true
+    cmd_Go.hidden = true
 }
 
 function evenement() {
@@ -36,7 +85,18 @@ function evenement() {
     BTN_1.addEventListener("click", div4YES);
     BTN_2.addEventListener("click", div4YES);
     texte.addEventListener("click", div5delete);
+    Txt_Name.addEventListener("keyup", divEx6)
+    cmd_Marks.addEventListener("click", div7Marks);
+    cmd_Ready.addEventListener("click", div7Ready);
+    cmd_Go.addEventListener("click", div7Go);
 }
+
+
+
+
+
+
+
 
 
 
