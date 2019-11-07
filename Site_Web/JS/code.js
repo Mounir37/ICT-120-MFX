a53f679dbfc190d4bc535eeea7120d93dc7bf6ee
Mounir37 <47849542+Mounir37@users.noreply.github.com>
2019-11-07 09:20:34 +0100

    travail anthony

--- Site_Web/JS/code.js
+++ Site_Web/JS/code.js
@@ -9,3 +9,19 @@ function init()
 {
     cmd_settings.addEventListener("click", settings);
 }
+
+$(document).ready(function () {
+
+    $('.first-button').on('click', function () {
+
+        $('.animated-icon1').toggleClass('open');
+    });
+    $('.second-button').on('click', function () {
+
+        $('.animated-icon2').toggleClass('open');
+    });
+    $('.third-button').on('click', function () {
+
+        $('.animated-icon3').toggleClass('open');
+    });
+});
